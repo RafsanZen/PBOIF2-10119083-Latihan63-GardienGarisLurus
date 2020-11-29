c323a8799a72fe5ce57243e600b60a4de9132f64
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-29 07:35:49 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan63-GardienGarisLurus/src/pboif2/pkg10119083/latihan63/gardiengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan63.gardiengarislurus;
+
+/**
+ *
+ * NAMA        : Rafsam Zen Mustaofa
+ * KELAS       : PBOIF2
+ * NIM         : 10119083
+ * Deskripsi   : program garis lurus
+ */
+public interface GarisLurus {
+     public int hitungGradien();
+}
